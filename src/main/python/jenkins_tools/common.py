3c10e46f0c88ca0844e815132f207d133a6218d6
Sunjoo Park <all4dich@gmail.com>
2019-07-18 11:46:20 +0900

    Add _flatted_job variable
    This variable have all jobs including child jobs under a folder until
    the first level

--- src/main/python/jenkins_tools/common.py
+++ src/main/python/jenkins_tools/common.py
@@ -2,6 +2,8 @@ import json
 import requests
 import logging
 
+from jenkins_tools.types import job_classes
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -20,6 +22,7 @@ class Jenkins:
         self._password = password
         self._auth = (self._username, self._password)
         self._jobs = []
+        self._flatted_jobs = []
 
     @property
     def url(self):
@@ -33,7 +36,28 @@ class Jenkins:
     def jobs(self):
         return self._jobs
 
+    def get_jobs(self):
+        """
+        Get a list of all jobs including items under 'Folder' object
+        :return: All jobs
+        """
+        self.check_connection()
+        self._flatted_jobs = []
+        for each_job in self._jobs:
+            self._flatted_jobs.append(each_job)
+            if each_job['_class'] == job_classes.folder:
+                r = self.get_object(each_job['url'])
+                jobs = r['jobs']
+                each_job['jobs'] = jobs
+                self._flatted_jobs = list(self._flatted_jobs + jobs)
+        return self._flatted_jobs
+
     def check_connection(self,tree=""):
+        """
+        Check if you can call Jenkins api with host and authentication information you provide
+        :param tree: Set the range of returned data from Jenkins like "&tree=jobs[name]"
+        :return: True if a connection is OK or False for other cases
+        """
         jenkins_api_url = f"{self._url}/api/json?pretty=true{tree}"
         logger.info(f"Call {jenkins_api_url}")
         res = requests.get(jenkins_api_url, auth=self._auth)
@@ -45,3 +69,18 @@ class Jenkins:
             return True
         else:
             return False
+
+    def get_object(self,url=""):
+        """
+        Get Jenkins object(job, view, folder, agent.. )'s information from the web UI url
+
+        :param url: Object's web UI url
+        :return: Json Object
+        """
+        api_url = f"{url}api/json"
+        res = requests.get(api_url, auth=self._auth)
+        if res.status_code != 200:
+            raise Exception(f"Call url = {api_url}, Error code = {res.status_code}. Check your request")
+        else:
+            res_json = json.loads(res.text)
+            return res_json
---
+++ src/main/python/jenkins_tools/types.py
@@ -0,0 +1,19 @@
+class JobClasses:
+    def __init__(self):
+        self._folder = "com.cloudbees.hudson.plugins.folder.Folder"
+        self._free_style = "hudson.model.FreeStyleProject",
+        self._workflow = "org.jenkinsci.plugins.workflow.job.WorkflowJob"
+
+    @property
+    def folder(self):
+        return self._folder
+
+    @property
+    def free_style(self):
+        return self._free_style
+
+    @property
+    def workflow(self):
+        return self._workflow
+
+job_classes = JobClasses()
